d4bfb47f90edcc78c4289500b392737b92743794
Sahilsrs45 <sahilranwade27@gmail.com>
2024-07-16 11:11:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import BasketballPlayerCard from './BasketballPlayerCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basketball Player Card</h1>
+      <BasketballPlayerCard
+        name="LeBron James"
+        image="https://i2.wp.com/nypost.com/wp-content/uploads/sites/2/2018/01/lebron-2.jpg?quality=90&strip=all&ssl=1"
+        position="Forward"
+        stats={{ pointsPerGame: 25.4, assistsPerGame: 7.1, reboundsPerGame: 10.5 }}
+      />
     </div>
   );
 }
